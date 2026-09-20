d99bb1819fcfbee755772700d1ef686c8e4df726
Dr_Tikas <1796654305@qq.com>
2026-09-20 19:31:40 +0800

    1.添加用例-正常修改用户信息 2.增加了报告id功能

--- testcase/Single Interface/test_debug_api.py
+++ testcase/Single Interface/test_debug_api.py
@@ -2,21 +2,29 @@ import allure
 import pytest
 
 from base.apiutil import RequestBase
+from common.generateId import m_id, c_id
 from common.readyaml import get_testcase_yaml
 
 
-@allure.feature('用户管理模块（单接口）')
+@allure.feature(next(m_id) + '用户管理模块（单接口）')
 class TestUserManager:
 
-    @allure.story("新增用户")
+    @allure.story(next(c_id) + "新增用户")
     @pytest.mark.run(order=1)
     @pytest.mark.parametrize('base_info,testcase', get_testcase_yaml('testcase/Single Interface/addUser.yaml'))
     def test_add_user(self, base_info, testcase):
         allure.dynamic.title(testcase['case_name'])
         RequestBase().specification_yaml(base_info, testcase)
 
-    @allure.story("查询用户")
+    @allure.story(next(c_id) + "修改用户")
     @pytest.mark.run(order=2)
+    @pytest.mark.parametrize('base_info,testcase', get_testcase_yaml('testcase/Single Interface/updateUser.yaml'))
+    def test_update_user(self, base_info, testcase):
+        allure.dynamic.title(testcase['case_name'])
+        RequestBase().specification_yaml(base_info, testcase)
+
+    @allure.story(next(c_id) + "查询用户")
+    @pytest.mark.run(order=4)
     @pytest.mark.parametrize('base_info, testcase', get_testcase_yaml('testcase/Single Interface/queryUser.yaml'))
     def test_query_user(self, base_info, testcase):
         allure.dynamic.title(testcase['case_name'])
