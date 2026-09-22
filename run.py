41ea9b6aa95246bd554d63d11f62753ad724d1f7
Dr_Tikas <1796654305@qq.com>
2026-09-22 19:59:08 +0800

    Jenkins适配

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 import os
 import shutil
+import sys
 
 import pytest
 
 if __name__ == '__main__':
-    pytest.main([ '-s', '-v', '--alluredir=./report/temp', './testcase', '--clean-alluredir',
-             '--junitxml=./report/results.xml'])
+    # pytest.main([ '-s', '-v', '--alluredir=./report/temp', './testcase', '--clean-alluredir',
+    #          '--junitxml=./report/results.xml'])
+
+    # 外部传入的所有参数交给pytest，不再写死参数
+    pytest.main(sys.argv[1:])
     shutil.copy('./environment.xml', './report/temp')
 
     # os.system(f'allure serve ./report/temp')
